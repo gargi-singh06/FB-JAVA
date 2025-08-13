fe7bceb7c7e3395bf1d423a230eca3a7b81476da
Gargi Singh <gargisinghmarch@gmail.com>
2025-08-13 09:21:53 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,7 @@
+ public class output{
+    public static void main(String[] args){
+        System.out.println("my name is: Gargi Singh");
+        System.out.println("class: B.tech 2nd year sec-FB");
+        System.out.print("ROLL NO- 16");
+    }
+}
