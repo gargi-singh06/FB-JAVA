7c0dee091c029b03745ba203a2b610a98a84ece4
Gargi Singh <gargisinghmarch@gmail.com>
2025-08-13 14:36:06 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array{
+    public static void main(String[]args){
+        int[]a={1,6,7,80};
+        System.out.println(a[0]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[1]);
+    }
+}
