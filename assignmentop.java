a371b6f7c8ba6f4e55c6e3ec8d8afb1cc435ccc7
Gargi Singh <gargisinghmarch@gmail.com>
2025-08-14 08:40:36 +0530

    Add files via upload

---
+++ assignmentop.java
@@ -0,0 +1,17 @@
+public class assignmentop{
+    public static void main(String[]args){
+       int a=5;
+        System.out.println(a);
+          a+=2;
+        System.out.println(a);
+          a=5;
+          a-=2;
+        System.out.println(a);
+          a=6;
+          a *= 2;
+        System.out.println(a);
+          a=5;
+          a/=2;
+        System.out.println(a);
+    }
+}
