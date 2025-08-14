7ec316b820efac74a2a4e7776861d4b2cf4e919b
Gargi Singh <gargisinghmarch@gmail.com>
2025-08-14 09:06:14 +0530

    Add files via upload

---
+++ swap2.java
@@ -0,0 +1,11 @@
+public class swap2{
+    public static void main(String[]args){
+        int a=5, b=8;
+         a=a+b;
+         b=a-b;
+         a=a-b;
+         System.out.println(a);
+         System.out.println(b);
+
+    }
+}
